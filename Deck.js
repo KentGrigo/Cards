f4b73c2ff7e8291dfda2edd0a54f2329176d9f70
Kent Grigo <kentgrigo@hotmail.com>
2021-08-24 21:17:15 +0200

    Auto-format

--- Deck.js
+++ Deck.js
@@ -2,21 +2,21 @@ function Deck(cards) {
     this.cards = cards;
 }
 
-Deck.prototype.get = function(index) {
+Deck.prototype.get = function (index) {
     return this.cards[index];
 }
 
-Deck.prototype.shuffle = function() {
+Deck.prototype.shuffle = function () {
     for (var i = this.cards.length - 1; 0 < i; i--) {
-	var shuffledCardIndex = randomNumberBetween(0, i + 1);
-	swap(this.cards, i, shuffledCardIndex);
+        var shuffledCardIndex = randomNumberBetween(0, i + 1);
+        swap(this.cards, i, shuffledCardIndex);
     }
 }
 
-Deck.prototype.print = function() {
+Deck.prototype.print = function () {
     for (var i = 0; i < this.cards.length; i++) {
-	var card = this.cards[i];
-	console.log(card);
+        var card = this.cards[i];
+        console.log(card);
     }
 }
 
@@ -35,7 +35,7 @@ function swap(collection, index1, index2) {
 function setupDeck(numberOfCards) {
     var cards = [];
     for (var i = 0; i < numberOfCards; i++) {
-	cards.push(i);
+        cards.push(i);
     }
     var deck = new Deck(cards);
     return deck;
@@ -49,35 +49,34 @@ function run() {
     deck.print();
 }
 
-
 function testFairness(numberOfRuns, numberOfCards) {
     var table = [];
     for (var i = 0; i < numberOfCards; i++) {
-	var row = [];
-	for (var j = 0; j < numberOfCards; j++) {
-	    row.push(0);
-	}
-	table.push(row);
+        var row = [];
+        for (var j = 0; j < numberOfCards; j++) {
+            row.push(0);
+        }
+        table.push(row);
     }
 
     for (var i = 0; i < numberOfRuns; i++) {
-	var deck = setupDeck(numberOfCards);
-	deck.shuffle();
-	for (var j = 0; j < numberOfCards; j++) {
-	    var card = deck.get(j);
-	    table[j][card]++;
-	}
+        var deck = setupDeck(numberOfCards);
+        deck.shuffle();
+        for (var j = 0; j < numberOfCards; j++) {
+            var card = deck.get(j);
+            table[j][card]++;
+        }
     }
 
     for (var i = 0; i < numberOfCards; i++) {
-	var row = "";
-	for (var j = 0; j < numberOfCards; j++) {
-	    var entry = "" + table[i][j];
-	    if (row != "") {
-		row += ", ";
-	    }
-	    row += entry;
-	}
-	console.log("Row #" + i + ": " + row);
+        var row = "";
+        for (var j = 0; j < numberOfCards; j++) {
+            var entry = "" + table[i][j];
+            if (row != "") {
+                row += ", ";
+            }
+            row += entry;
+        }
+        console.log("Row #" + i + ": " + row);
     }
 }
